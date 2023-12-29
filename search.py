b311b6eb327a5d4667088d3679a9b042520ceee6
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-12-29 22:34:24 +0700

    search: find param

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -17,6 +17,7 @@ def load_dataset(file_path):
 
 
 def process_text(text):
+    # Add your regex-based preprocessing steps here
     text = re.sub('\b[\w\-.]+?@\w+?\.\w{2,4}\b', 'emailaddr', text)
     text = re.sub('(http[s]?\S+)|(\w+\.[A-Za-z]{2,4}\S*)', 'httpaddr', text)
     text = re.sub('£|\$', 'moneysymb', text)
@@ -24,20 +25,29 @@ def process_text(text):
         '\b(\+\d{1,2}\s)?\d?[\-(.]?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b',
         'phonenumbr', text)
     text = re.sub('\d+(\.\d+)?', 'numbr', text)
-
+    # Remove all punctuations
     text = re.sub('[^\w\d\s]', ' ', text)
 
+    # Each word to lower case
     text = text.lower()
 
+    # Splitting words to Tokenize
     text = text.split()
     return ' '.join(text)
 
 
 def main():
+    # Load dataset
     dataset = load_dataset('spam_added.csv')
 
+    # Display class distribution
+    label_counts = dataset["Label"].value_counts()
+    print("Class Distribution:", label_counts)
+
+    # Preprocess messages
     dataset["ProcessedText"] = dataset["EmailText"].apply(process_text)
 
+    # Train-test split
     X_train, X_test, y_train, y_test = train_test_split(
         dataset["ProcessedText"],
         dataset["Label"].values,
@@ -50,30 +60,39 @@ def main():
           np.sum(y_train == "ham") / np.sum(y_train == "spam"))
     print("Test Set Class Distribution:",
           np.sum(y_test == "ham") / np.sum(y_test == "spam"))
-    print("TEST PROCESING:\n", process_text(dataset["EmailText"].tolist()[2]))
 
     # TF-IDF Vectorization and Random Forest Classifier in a Pipeline
     pipeline = Pipeline([('tfidf',
                           TfidfVectorizer(min_df=5,
                                           max_df=0.75,
-                                          preprocessor=None,
-                                          max_features=1000)),
-                         ('rf',
-                          RandomForestClassifier(max_depth=None,
-                                                 n_estimators=150,
-                                                 random_state=42))])
+                                          preprocessor=None)),
+                         ('rf', RandomForestClassifier())])
 
-    # Fit the pipeline on the training data
-    pipeline.fit(X_train, y_train)
+    # Define the parameter grid for hyperparameter tuning
+    param_grid = {
+        'tfidf__max_features': [1000, 2000, 3000],
+        'rf__n_estimators': [50, 100, 150],
+        'rf__max_depth': [None, 10, 20],
+        'rf__random_state': [42]
+    }
 
-    # Get the number of features
-    num_features = len(pipeline.named_steps['tfidf'].get_feature_names_out())
-    print("Number of Features:", num_features)
+    # Use GridSearchCV to find the best parameters
+    grid_search = GridSearchCV(pipeline,
+                               param_grid,
+                               cv=5,
+                               scoring='accuracy',
+                               verbose=1)
+    grid_search.fit(X_train, y_train)
 
-    # Model Evaluation
-    y_pred = pipeline.predict(X_test)
+    # Print the best parameters and best accuracy
+    print("Best Parameters:", grid_search.best_params_)
+    print("Best Accuracy:", grid_search.best_score_)
+
+    # Model Evaluation with best parameters
+    best_model = grid_search.best_estimator_
+    y_pred = best_model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
-    print("Test Set Accuracy:", accuracy)
+    print("Test Set Accuracy with Best Parameters:", accuracy)
 
     while True:
         msg = input("Input your message:\n")
@@ -81,9 +100,8 @@ def main():
             break
 
         processed_msg = process_text(msg)
-        print("PROCESSED:\n", processed_msg)
         real_time = [processed_msg]
-        prediction = pipeline.predict(real_time)
+        prediction = best_model.predict(real_time)
 
         print("Prediction:", prediction[0])
 
